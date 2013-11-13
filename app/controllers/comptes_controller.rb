266fb4901f831a620b3582b4c7edc07814be43a3
jerome <doc.jerome.attia@gmail.com>
2013-11-13 16:59:02 +0100

    tabs in index

--- app/controllers/comptes_controller.rb
+++ app/controllers/comptes_controller.rb
@@ -10,6 +10,7 @@ class ComptesController < ApplicationController
     @liste2013 = Compte.where(mois: ("2013-01-01".."2013-12-31")).each.map{|cp| [cp.mois.strftime("%b"), cp.valeur.to_f]}
     @liste2012 = Compte.where(mois: ("2012-01-01".."2012-12-31")).each.map{|cp| [cp.mois.strftime("%b"), cp.valeur.to_f]}
     @liste2011 = Compte.where(mois: ("2011-01-01".."2011-12-31")).each.map{|cp| [cp.mois.strftime("%b"), cp.valeur.to_f]}
+    @liste2010 = Compte.where(mois: ("2010-01-01".."2010-12-31")).each.map{|cp| [cp.mois.strftime("%b"), cp.valeur.to_f]}
   end
 
   def list
